7681aac36566e7066c7b4e77249dc2bd2b80814f
Nicholas Jamieson <nicholas@cartant.com>
2017-10-02 09:51:50 +1000

    docs(license): Update comments.

--- source/index.ts
+++ source/index.ts
@@ -1,5 +1,5 @@
 /**
- * @license Copyright © 2017 Nicholas Jamieson. All Rights Reserved.
+ * @license Copyright © 2017 Nicholas Jamieson et al. All Rights Reserved.
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://github.com/cartant/ts-snippet
  */
